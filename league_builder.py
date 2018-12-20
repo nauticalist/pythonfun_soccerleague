a3de238f4daa956e3e54c3bb5adeb7968cb36154
Ismail Aksoy <nauticalist@solaris.seanapse.io>
2018-12-20 18:02:37 +0300

    create teams function

--- league_builder.py
+++ league_builder.py
@@ -1,14 +1,36 @@
 import csv
 
+teams = {"Sharks": [], "Dragons": [], "Raptors": []}
+
 
 def get_players_from_csv():
     with open('soccer_players.csv', newline='') as csvfile:
         players = csv.DictReader(csvfile, delimiter=",")
-        rows = list(players)
-        return rows
+        experienced_players = []
+        noob_players = []
+        for player in players:
+            if player['Soccer Experience'] == 'YES':
+                experienced_players.append(player)
+            else:
+                noob_players.append(player)
+        return {'experienced players': experienced_players,
+                'noob players': noob_players}
+
+
+def create_teams(teams, players):
+    experienced_players = players['experienced players']
+    noob_players = players['noob players']
+    experienced_players_per_team = len(experienced_players) // len(teams)
+    noob_players_per_team = len(noob_players) // len(teams)
+    for team, players in teams.items():
+        for i in range(experienced_players_per_team):
+            players.append(experienced_players.pop(0))
+        for i in range(noob_players_per_team):
+            players.append(noob_players.pop(0))
+    for team, players in teams.items():
+        print(team, ': ', players)
 
 
 if __name__ == '__main__':
     players = get_players_from_csv()
-    for player in players:
-        print(player['Name'])
+    create_teams(teams, players)
